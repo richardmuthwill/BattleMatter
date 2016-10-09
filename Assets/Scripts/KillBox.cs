5f1ac94cfc733e0eb3631f9461d2eeb6a4b79100
richardmuthwill-CV <cv@muthwill-it.com>
2016-10-09 10:16:47 +0200

    Added "KillBox" in TDM1

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+// https://docs.unity3d.com/Manual/UNetSetup.html
+public class KillBox : NetworkBehaviour {
+
+	private const string PLAYER_TAG = "Player";
+	private BoxCollider collider;
+
+	public int damage = 999999;
+
+	void Start ()
+	{
+		collider = gameObject.GetComponent<BoxCollider> ();
+	}
+
+	void OnTriggerEnter (Collider collision)
+	{
+		LooseHealth (collision);
+	}
+
+	[Client]
+	void LooseHealth (Collider col)
+	{
+		if (col.GetComponent<Collider>().tag == PLAYER_TAG) {
+			CmdLooseHealth (col.GetComponent<Collider>().name, damage);
+		}
+	}
+
+	[Command]
+	void CmdLooseHealth (string _playerID, int _damage)
+	{
+		Debug.Log (_playerID + " fell to their death");
+
+		Player _player = GameManager.GetPlayer(_playerID);
+		_player.RpcTakeDamage (_damage);
+	}
+}
